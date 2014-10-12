1e2319107a122708fee2637bd3af7422c49589c2
renan <rsantos.dev@gmail.com>
2014-10-12 14:21:07 -0300

    the structure was changed to parse

---
+++ MyFirstTravel/src/main/java/br/com/epicdroid/travel/constants/ParseConstants.java
@@ -0,0 +1,8 @@
+package br.com.epicdroid.travel.constants;
+
+public class ParseConstants {
+    public static final String MY_FIRST_TRAVEL = "my_first_travel";
+
+    public static final String PARSE_APP_ID = "ZgxI2Z3DzLvoiHe4boGmFN0pG9tXnQEo3j1vQkM2";
+    public static final String PARSE_CLIENT_KEY = "ocmzx4dPy2ngU5jaUyd4KbUnqEY628sVbRSTYBfp";
+}
